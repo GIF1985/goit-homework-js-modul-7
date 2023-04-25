3f1027e8ddbb70c83871ce9e4af60feee9a3b515
GIF1985 <125868361+GIF1985@users.noreply.github.com>
2023-04-25 13:56:20 +0300

    на проверку

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,73 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// Импортируем массив объектов с данными о картинках
+import { galleryItems } from "./gallery-items.js";
+
+// Находим элемент галереи на странице
+const galleryRef = document.querySelector(".gallery");
+
+// Создаем HTML-разметку для каждого элемента галереи
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+// Добавляем HTML-разметку в элемент галереи
+galleryRef.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+// Функция для создания HTML-разметки для каждого элемента галереи
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      //элементы галереи
+      return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+        </a>
+      </li>
+    `;
+    })
+    .join("");
+}
+
+// Добавляем обработчик клика на элементы галереи
+galleryRef.addEventListener("click", onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+
+  const target = event.target;
+
+  // Проверяем, что клик был произведен на элементе img
+  if (target.nodeName !== "IMG") {
+    return;
+  }
+
+  // Получаем URL большого изображения из атрибута data-source
+  const largeImageURL = target.dataset.source;
+
+  //     const instance = basicLightbox.create(`
+  //     <img src="assets/images/image.png" width="800" height="600">
+  // `);
+
+  //     instance.show();
+
+  // Создаем экземпляр модального окна с большим изображением и с заменной значения атрибута src
+  const instance = basicLightbox.create(`
+    <img src="${largeImageURL}" width="800" height="600">
+`);
+
+  // Открываем модальное окно с большим изображением
+  instance.show();
+
+  // Запрещаем перенаправление на другую страницу при клике на изображение
+  const link = instance.element().querySelector("img");
+  link.addEventListener("click", (event) => event.preventDefault());
+
+  // Добавляем обработчик события нажатия на клавишу Escape
+  const handleKeyPress = (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+      document.removeEventListener("keydown", handleKeyPress);
+    }
+  };
+  document.addEventListener("keydown", handleKeyPress);
+}
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// Импортируем массив объектов с данными о картинках
+import { galleryItems } from "./gallery-items.js";
+
+// Функция для создания и рендеринга разметки галереи
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        //элементы галереи
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+                <div class="gallery__caption">${description}</div>
+            </a>
+        </li>`
+    )
+    .join("");
+}
+
+// Получаем ссылку на элемент ul.gallery
+const gallery = document.querySelector(".gallery");
+
+// Создаем и добавляем разметку галереи в ul.gallery
+const galleryMarkup = createGalleryMarkup(galleryItems);
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+// Инициализация SimpleLightbox
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
